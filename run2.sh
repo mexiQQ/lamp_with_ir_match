f1f55d518c556178402bb146c8156c4febceca68
mexiqq <ljw040426@gmail.com>
2023-07-02 20:49:35 +0000

    first commit

---
+++ run2.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=2 ./lamp_cos2.sh /hdd1/jianwei/workspace/lamp/models/bert-base-finetuned-sst2 sst2 8
+
---
+++ run3.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=3 ./lamp_cos3.sh /hdd1/jianwei/workspace/lamp/models/bert-base-finetuned-sst2 sst2 1 
+
