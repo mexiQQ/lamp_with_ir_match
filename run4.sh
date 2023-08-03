ef282cf6488e6d964a343fc64fa6b70e254c3cc1
mexiqq <ljw040426@gmail.com>
2023-08-03 05:22:41 +0000

    update

---
+++ run4.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1 ./lamp_cos4.sh /hdd1/jianwei/workspace/lamp/models/bert-base-finetuned-sst2 sst2 8 
+# CUDA_VISIBLE_DEVICES=3 ./lamp_cos3.sh /hdd1/jianwei/workspace/lamp/models/bert-base-uncased sst2 2
+# CUDA_VISIBLE_DEVICES=2 ./lamp_cos3.sh /hdd1/jianwei/workspace/lamp/outputs/finetune_glue-sst2_lr2e-05_epochs1_seed302_time1690088507223 sst2 2
+
